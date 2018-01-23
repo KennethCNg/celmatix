b45510b3dddfd418e5243e8f7de1a23b196ed3a8
Kenneth Ng <kenneth.ng.4@gmail.com>
2018-01-23 02:20:33 -0500

    fix radio button not keeping state

--- frontend/color_form.jsx
+++ frontend/color_form.jsx
@@ -10,13 +10,13 @@ const ColorForm = props => {
         <Form onSubmit={ props.handleColorVerification }>
             <ControlLabel>Favorite Color</ ControlLabel>
             <FormGroup onChange={props.setColor}>
-                <Radio defaultChecked value="Red" name="colorGroup">Red</Radio>
-                <Radio value="Orange" name="colorGroup">Orange</Radio>
-                <Radio value="Yellow" name="colorGroup">Yellow</Radio>
-                <Radio value="Green" name="colorGroup">Green</Radio>
-                <Radio value="Blue" name="colorGroup">Blue</Radio>
-                <Radio value="Purple" name="colorGroup">Purple</Radio>
-                <Radio value=""name="colorGroup">Other</Radio>
+                <Radio checked={props.color === "Red"} value="Red" name="colorGroup">Red</Radio>
+                <Radio value="Orange" checked={props.color === "Orange"} name="colorGroup">Orange</Radio>
+                <Radio value="Yellow" checked={props.color === "Yellow"} name="colorGroup">Yellow</Radio>
+                <Radio value="Green" checked={props.color === "Green"} name="colorGroup">Green</Radio>
+                <Radio value="Blue" checked={props.color === "Blue"}name="colorGroup">Blue</Radio>
+                <Radio value="Purple" checked={props.color === "Purple"} name="colorGroup">Purple</Radio>
+                <Radio value="" name="colorGroup" checked={!(defaultColors.has(props.color) )} >Other</Radio>
                 <FormControl readOnly={defaultColors.has(props.color)} type="text" value={props.color} onChange={props.handleChange('color')}/>
             </ FormGroup>
             <br />
