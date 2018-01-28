6be9b85ca6e866a1a16d834dfe907d8e7201b3d2
Kenneth Ng <kenneth.ng.4@gmail.com>
2018-01-27 22:25:09 -0500

    saving over testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
     def create
         user = User.new(user_params)
-        debugger
         if user.save
         # do nothing, great!
         else
